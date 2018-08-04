5977c35129a149e935c732588d6503cd1d58ea45
Emran Ahmed <emran.bd.08@gmail.com>
2018-08-04 13:48:29 +0600

    update

--- woo-variation-swatches.php
+++ woo-variation-swatches.php
@@ -150,7 +150,7 @@
 				// Don't load JS on IE11
 				if ( ! wvs_is_ie11() ) {
 					wp_enqueue_script( 'woo-variation-swatches', $this->assets_uri( "/js/frontend{$suffix}.js" ), array( 'jquery' ), $this->version(), true );
-					wp_localize_script( 'woo-variation-swatches', 'woo_variation_swatches_options', apply_filters( 'woo_variation_swatches_options', array(
+					wp_localize_script( 'woo-variation-swatches', 'woo_variation_swatches_options', apply_filters( 'woo_variation_swatches_js_options', array(
 						'is_product_page' => is_product(),
 					) ) );
 				}
