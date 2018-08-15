a58bc76abd23f26ba791602ecc0b8880f2a724c3
Ahmed Eshaan <eshaanfaysal@gmail.com>
2018-08-15 22:42:48 +0600

    Link updated

--- includes/tutorials.php
+++ includes/tutorials.php
@@ -1,5 +1,5 @@
 <?php
-	defined( 'ABSPATH' ) or die( 'Keep Quit' );
+    defined( 'ABSPATH' ) or die( 'Keep Quit' );
 ?>
 
 <div class="gwp-tutorials-wrapper">
@@ -13,8 +13,8 @@
                     The option turns product attribute variation select options drop down into color swatches. It’s the best fit options for the variable products comes with multiple attribute variations.
                 </div>
                 <div class="tutorial-buttons">
-                    <a href="#" class="button button-live-demo">Live Demo</a>
-                    <a href="#" class="button button-docs">Documentation</a>
+                    <a href="http://bit.ly/tutorial-tab-01" target="_blank" class="button button-live-demo">Live Demo</a>
+                    <a href="#" target="_blank" class="button button-docs">Documentation</a>
                 </div>
             </div>
         </li>
@@ -27,8 +27,8 @@
                     Images variation does more than color swatches. When it comes to display images as product variation, this option comes handy and highly engaging for conversion.
                 </div>
                 <div class="tutorial-buttons">
-                    <a href="#" class="button button-live-demo">Live Demo</a>
-                    <a href="#" class="button button-docs">Documentation</a>
+                    <a href="http://bit.ly/tutorial-tab-02" target="_blank" class="button button-live-demo">Live Demo</a>
+                    <a href="#" target="_blank" class="button button-docs">Documentation</a>
                 </div>
             </div>
             <div class="tutorial-image-wrapper"><img alt="" src="<?php echo woo_variation_swatches()->images_uri( 'tutorial-2.png' ) ?>"></div>
@@ -37,13 +37,56 @@
         <li>
             <div class="tutorial-image-wrapper"><img alt="" src="<?php echo woo_variation_swatches()->images_uri( 'tutorial-3.png' ) ?>"></div>
             <div class="tutorial-description-wrapper">
-                <h3>Color Swatches For Attribute Variation</h3>
+                <h3>Button/Label Swatches For Attribute Variation</h3>
                 <div class="tutorial-contents">
-                    The option turns product attribute variation select options drop down into color swatches. It’s the best fit options for the variable products comes with multiple attribute variations.
+                    When comes to show available product size, quantity and other variation related details, button/label swatch boost conversion extensively. It allows selecting customers to select their desired product variation quickly.
                 </div>
                 <div class="tutorial-buttons">
-                    <a href="#" class="button button-live-demo">Live Demo</a>
-                    <a href="#" class="button button-docs">Documentation</a>
+                    <a href="http://bit.ly/tutorial-tab-03" target="_blank" class="button button-live-demo">Live Demo</a>
+                    <a href="#" target="_blank" class="button button-docs">Documentation</a>
+                </div>
+            </div>
+        </li>
+
+        <li>
+            <div class="tutorial-image-wrapper"><img alt="" src="<?php echo woo_variation_swatches()->images_uri( 'tutorial-4.png' ) ?>"></div>
+            <div class="tutorial-description-wrapper">
+                <h3>Integrated With Quick View</h3>
+                <div class="tutorial-contents">
+                   Besides showing swatches on product details page and archive pages, it can enable swatches on quickview lighboxes to maximize store wide sales.
+                </div>
+                <div class="tutorial-buttons">
+                    <a href="http://bit.ly/tutorial-tab-04" target="_blank" class="button button-live-demo">Live Demo</a>
+                    <a href="#" target="_blank" class="button button-docs">Documentation</a>
+                </div>
+            </div>
+        </li>
+
+
+        <li>
+            <div class="tutorial-description-wrapper">
+                <h3>Text Tooltip</h3>
+                <div class="tutorial-contents">
+                   Tooltip denotes the variation details to explain more. It can be disabled and customized the title text and tooltip background from the admin backend.
+                </div>
+                <div class="tutorial-buttons">
+                    <a href="http://bit.ly/tutorial-tab-06" target="_blank" class="button button-live-demo">Live Demo</a>
+                    <a href="#" target="_blank" class="button button-docs">Documentation</a>
+                </div>
+            </div>
+            <div class="tutorial-image-wrapper"><img alt="" src="<?php echo woo_variation_swatches()->images_uri( 'tutorial-5.png' ) ?>"></div>
+        </li>
+
+        <li>
+            <div class="tutorial-image-wrapper"><img alt="" src="<?php echo woo_variation_swatches()->images_uri( 'tutorial-6.png' ) ?>"></div>
+            <div class="tutorial-description-wrapper">
+                <h3>Blur/Hide Out of Stock Variation</h3>
+                <div class="tutorial-contents">
+                    Showing out of stock variation seems unnecessary. So, the plugin offers option to blur or hide the out of stock variation to simplify in stock variation.
+                </div>
+                <div class="tutorial-buttons">
+                    <a href="http://bit.ly/tutorial-tab-05" target="_blank" class="button button-live-demo">Live Demo</a>
+                    <a href="#" target="_blank" class="button button-docs">Documentation</a>
                 </div>
             </div>
         </li>
